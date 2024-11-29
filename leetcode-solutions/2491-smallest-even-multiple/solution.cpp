ba3a9ab2932e9f3f6580f1d6ef606c77a9742069
Prasanth-tesla <prasanthcse786@gmail.com>
2024-11-29 12:58:38 +0000

    Sync LeetCode submission - Smallest Even Multiple - Runtime - 0 ms (100.00%), Memory - 8 MB (0.55%)

---
+++ leetcode-solutions/2491-smallest-even-multiple/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n%2==0)
+        printf("%d",n);
+        else
+        n=n*2;
+        printf("%d",n);
+        return n;
+        
+    }
+};
