1aae020b2043b80b4ceb78c6d56745f09cf9487b
Prasanth-tesla <prasanthcse786@gmail.com>
2025-06-27 11:59:01 +0000

    Sync LeetCode submission - Three Divisors - Runtime - 1 ms (74.37%), Memory - 12.6 MB (47.49%)

---
+++ leetcode-solutions/2083-three-divisors/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isThree(self, n):
+        if 1<=n<=10**4:
+            div=0
+            for i in range(1,n+1):
+                if n%i==0:
+                   div+=1
+            return div==3
+        return 0
+        
