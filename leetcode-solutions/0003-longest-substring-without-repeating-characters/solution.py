5aeaadd37534a85ddd7cb1ad0820db6e3433efc1
Prasanth-tesla <prasanthcse786@gmail.com>
2025-10-03 04:13:50 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters - Runtime - 21 ms (66.55%), Memory - 13.2 MB (46.55%)

---
+++ leetcode-solutions/0003-longest-substring-without-repeating-characters/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        left = 0
+        maxLen = 0
+
+        for right in range (len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            maxLen = max(maxLen , right - left + 1)
+
+        return maxLen
+            
