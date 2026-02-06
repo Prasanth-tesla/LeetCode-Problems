f3492d4d7983260eb8c9e60245adba53985debf1
Prasanth-tesla <prasanthcse786@gmail.com>
2026-02-06 05:58:37 +0000

    Sync LeetCode submission - Remove Element - Runtime - 1 ms (9.13%), Memory - 12.4 MB (16.12%)

---
+++ leetcode-solutions/0027-remove-element/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k = 0
+        for i in nums:
+            if i != val:
+                nums[k] = i
+                k += 1
+        nums = sorted(nums[:k])
+        return k
