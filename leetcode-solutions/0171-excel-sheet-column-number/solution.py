1c0474fccc72fdea6288144e22eb5c882003b780
Prasanth-tesla <prasanthcse786@gmail.com>
2026-03-26 18:13:17 +0000

    Sync LeetCode submission - Excel Sheet Column Number - Runtime - 0 ms (100.00%), Memory - 12.4 MB (52.46%)

---
+++ leetcode-solutions/0171-excel-sheet-column-number/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        res = 0
+        for ch in columnTitle:
+            res = res * 26 + (ord(ch) - ord('A') + 1)
+        return res
