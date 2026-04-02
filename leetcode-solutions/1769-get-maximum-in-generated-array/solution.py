b0e665aa6c17d6d9b05feeddda1855be72663c58
Prasanth-tesla <prasanthcse786@gmail.com>
2026-04-02 19:35:54 +0000

    Sync LeetCode submission - Get Maximum in Generated Array - Runtime - 0 ms (100.00%), Memory - 12.3 MB (88.57%)

---
+++ leetcode-solutions/1769-get-maximum-in-generated-array/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def getMaximumGenerated(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+
+        nums = [0] * (n + 1)
+        nums[1] = 1
+
+        for i in range(n + 1):
+            if 2 <= 2 * i <= n:
+                nums[2 * i] = nums[i]
+            if 2 <= 2 * i + 1 <= n:
+                nums[2 * i + 1] = nums[i] + nums[i + 1]
+
+        return max(nums)
