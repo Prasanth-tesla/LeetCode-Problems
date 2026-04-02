b360894712e3ae997c3aa381157af46115a8abb9
Prasanth-tesla <prasanthcse786@gmail.com>
2026-04-02 15:14:13 +0000

    Sync LeetCode submission - Add Binary - Runtime - 11 ms (11.22%), Memory - 12.3 MB (59.18%)

---
+++ leetcode-solutions/0067-add-binary/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i, j = len(a) - 1, len(b) - 1
+        carry, res = 0, ""
+
+        while i >= 0 or j >= 0 or carry != 0:
+            sum = carry
+            if i >= 0:
+                sum += int(a[i])
+                i -= 1
+            if j >= 0:
+                sum += int(b[j])
+                j -= 1
+            res += str(sum % 2)
+            carry = sum // 2
+        return res[::-1]
+
+
