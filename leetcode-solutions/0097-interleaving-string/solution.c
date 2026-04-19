a29670a1fef3912aeccc74b1cd58639902f209ee
Prasanth-tesla <prasanthcse786@gmail.com>
2026-04-19 18:45:17 +0000

    Sync LeetCode submission - Interleaving String - Runtime - 0 ms (100.00%), Memory - 9 MB (27.17%)

---
+++ leetcode-solutions/0097-interleaving-string/solution.c
@@ -0,0 +1,36 @@
+bool isInterleave(char* s1, char* s2, char* s3) {
+    int m = strlen(s1), n = strlen(s2);
+
+    if (m + n != strlen(s3)) return false;
+
+    bool **dp = (bool **)malloc((m + 1) * sizeof(bool *));
+    for (int i = 0; i <= m; i++) {
+        dp[i] = (bool *)malloc((n + 1) * sizeof(bool));
+    }
+
+    dp[0][0] = true;
+
+    for (int i = 1; i <= m; i++) {
+        dp[i][0] = dp[i - 1][0] && (s1[i - 1] == s3[i - 1]);
+    }
+
+    for (int j = 1; j <= n; j++) {
+        dp[0][j] = dp[0][j - 1] && (s2[j - 1] == s3[j - 1]);
+    }
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+                       (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+        }
+    }
+
+    bool result = dp[m][n];
+
+    for (int i = 0; i <= m; i++) {
+        free(dp[i]);
+    }
+    free(dp);
+
+    return result;
+}
