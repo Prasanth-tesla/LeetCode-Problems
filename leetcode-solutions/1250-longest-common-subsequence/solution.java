d4b37553d2ee8b04efab9f8242dbc70940a5515b
Prasanth-tesla <prasanthcse786@gmail.com>
2026-07-21 09:35:19 +0000

    Sync LeetCode submission - Longest Common Subsequence - Runtime - 19 ms (91.59%), Memory - 53.6 MB (88.20%)

---
+++ leetcode-solutions/1250-longest-common-subsequence/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int row = text1.length();
+        int column = text2.length();
+
+        int [][] dp = new int[row + 1][column + 1];
+
+        for(int i = 1;i <= row;i++) {
+            for(int j = 1;j <= column;j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[row][column];
+    }
+}
