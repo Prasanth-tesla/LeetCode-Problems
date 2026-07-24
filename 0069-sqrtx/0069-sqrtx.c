c3409bf36b987cfef2ff6fa9f3903f58b93d08b6
Prasanth-tesla <prasanthcse786@gmail.com>
2026-07-24 10:25:24 +0530

    Time: 0 ms (100%), Space: 9.2 MB (55.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,13 @@
+int mySqrt(int x) {
+    int low = 0, high = x;
+
+        while(low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if(mid * mid == x) return mid;
+            else if(mid * mid < x) low = mid + 1;
+            else high = mid - 1;
+
+        }
+        return high;
+}
