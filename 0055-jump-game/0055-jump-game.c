699758f71cfd6e5d638db13114a6b958e3e5ebaf
PRASANTH <prasanthcse786@gmail.com>
2026-08-15 21:23:11 +0530

    Time: 1 ms (25.86%), Space: 11.4 MB (5.16%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.c
@@ -0,0 +1,9 @@
+bool canJump(int* nums, int numsSize) {
+    int maxReach = 0;
+    for(int i = 0 ; i < numsSize ; i++) {
+        if (i > maxReach)
+            return false;
+        maxReach = fmax(maxReach, i + nums[i]);
+    }
+    return true;
+}
