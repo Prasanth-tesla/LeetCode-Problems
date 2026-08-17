baad5835b0739e5efbf0f457d807bf649b227a9d
PRASANTH <prasanthcse786@gmail.com>
2026-08-17 14:12:22 +0530

    Time: 3 ms (73.11%), Space: 10 MB (51.66%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.c
@@ -0,0 +1,21 @@
+int maxVowels(char* s, int k) {
+    int count = 0;
+    
+    for(int i = 0 ; i < k ; i++) {
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            count++;
+    }
+
+    int maxCount = count;
+
+    for(int i = k ; s[i] != '\0' ; i++) {
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            count++;
+
+        if(s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' || s[i - k] == 'o' || s[i - k] == 'u') count--;
+
+        maxCount = fmax(maxCount, count);
+    }
+
+    return maxCount;
+}
