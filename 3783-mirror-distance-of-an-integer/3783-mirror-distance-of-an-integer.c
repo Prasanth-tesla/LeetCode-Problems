39cf0182972de957c0daf4499d3b2c5542666dba
PRASANTH <prasanthcse786@gmail.com>
2026-08-19 09:48:30 +0530

    Time: 0 ms (100%), Space: 9.3 MB (36.86%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.c
@@ -0,0 +1,10 @@
+int mirrorDistance(int n) {
+    int rev = 0, num = n;
+    while(num != 0) {
+        rev *= 10;
+        rev += num % 10;
+        num /= 10;
+    }
+
+    return n > rev ? n - rev : rev - n;
+}
