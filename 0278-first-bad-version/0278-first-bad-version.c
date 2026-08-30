cf2f03da3d73b0cffd1d08485e318bb4e079bf95
PRASANTH <prasanthcse786@gmail.com>
2026-08-30 12:45:40 +0530

    Time: 1 ms (58.66%), Space: 8.5 MB (30.49%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.c
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int low = 1, high = n, ans = 0;
+
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(isBadVersion(mid)) {
+            ans = mid;
+            high = mid - 1;
+        } else low = mid + 1;
+    }
+
+    return ans;
+}
