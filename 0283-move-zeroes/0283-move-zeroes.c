970d967c82fd8aaaa2d6782e15dc635bd8f7b227
PRASANTH <prasanthcse786@gmail.com>
2026-08-30 16:42:34 +0530

    Time: 0 ms (100%), Space: 20.5 MB (9.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,13 +1,14 @@
 void moveZeroes(int* nums, int numsSize) {
-    int insPos = 0;
+    int left = 0, right = 1;
 
-    for(int i = 0 ; i < numsSize ; i++) {
-        if(nums[i] != 0) {
-            nums[insPos] = nums[i];
-            insPos++;
+    while(right < numsSize) {
+        if(nums[left] != 0) left++;
+        else if(nums[right] != 0) {
+            nums[left] = nums[left] ^ nums[right];
+            nums[right] = nums[left] ^ nums[right];
+            nums[left] = nums[left] ^ nums[right];
+            left++;
         }
+        right++;
     }
-
-    for(int i = insPos ; i < numsSize ; i++)
-        nums[i] = 0;
 }
