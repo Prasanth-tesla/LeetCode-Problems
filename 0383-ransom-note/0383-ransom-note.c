4180e2c2498fdd119f3bc57406f050daa058c703
PRASANTH <prasanthcse786@gmail.com>
2026-09-01 15:09:50 +0530

    Time: 4 ms (16.12%), Space: 9.5 MB (35.01%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.c
@@ -0,0 +1,14 @@
+bool canConstruct(char* ransomNote, char* magazine) {
+    int* count = calloc(26, sizeof(int));
+
+    for(int i = 0; magazine[i] != '\0'; i++)
+        count[magazine[i] - 'a']++;
+
+    for(int i = 0; ransomNote[i] != '\0'; i++) {
+        if(count[ransomNote[i] - 'a'] == 0)
+            return false;
+        count[ransomNote[i] - 'a']--;
+    }
+    
+    return true;
+}
