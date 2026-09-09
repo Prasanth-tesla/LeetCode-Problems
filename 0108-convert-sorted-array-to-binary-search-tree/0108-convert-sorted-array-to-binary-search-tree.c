bbceb9927f70c6c3d2375170cc57b675255f7b95
PRASANTH <prasanthcse786@gmail.com>
2026-09-09 09:46:33 +0530

    Time: 3 ms (67.72%), Space: 16.6 MB (71.14%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* buildTree(int* nums, int start, int end) {
+    if(start > end) return NULL;
+
+    int mid = start + (end - start) / 2;
+    struct TreeNode* node = malloc(sizeof(struct TreeNode));
+    node -> val = nums[mid];
+
+    node -> left = buildTree(nums, start, mid - 1);
+    node -> right = buildTree(nums, mid + 1, end);
+
+    return node;
+} 
+
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    return buildTree(nums, 0, numsSize - 1);
+}
+
