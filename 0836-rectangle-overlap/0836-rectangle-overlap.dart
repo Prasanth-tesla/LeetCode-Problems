7a981c7493a56590a61bf6969f2a539a9af76ec4
PRASANTH <prasanthcse786@gmail.com>
2026-09-23 21:03:10 +0530

    Time: 0 ms (100%), Space: 147.8 MB (73.91%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.dart
@@ -0,0 +1,5 @@
+class Solution {
+  bool isRectangleOverlap(List<int> rec1, List<int> rec2) {
+    return (rec1[0] < rec2[2] && rec2[0] < rec1[2]) && (rec1[1] < rec2[3] && rec2[1] < rec1[3]);
+  }
+}
